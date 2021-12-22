f02c0f9462116da3654dc82849b230423757218b
Charlie H <charltonhua@gmail.com>
2021-12-22 00:53:02 -0800

    9.2.4

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
 // const printProfileData = (profileDataArr) => {
 //   // this..
@@ -15,3 +18,23 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 // };
 
 // printProfileData(profileDataArgs);
+
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portfolio Demo</title>
+</head>
+<body>
+  <h1>${name}</h1>
+  <h2><a href="https://github.com/${github}">Github</a></h2>;
+</body>
+</html>
+`;
+};
+
+console.log(generatePage(name, github));
