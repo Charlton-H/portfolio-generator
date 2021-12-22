ff7d4d372090a40db43c39c8c317ebba5a46dfa1
Charlie H <charltonhua@gmail.com>
2021-12-22 01:10:14 -0800

    9.2.6

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,5 @@
-
+const generatePage = (name, github) => {
+  return `
   <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +9,11 @@
   <title>Portfolio Demo</title>
 </head>
 <body>
-  <h1>jack</h1>
-  <h2><a href="https://github.com/jackhub">Github</a></h2>;
+  <h1>${name}</h1>
+  <h2><a href="https://github.com/${github}">Github</a></h2>;
 </body>
 </html>
+`;
+};
+
+module.exports = generatePage;
