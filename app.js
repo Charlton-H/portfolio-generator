7a892ebf361ef5686361483edb307fe8b7c145c7
Charlie H <charltonhua@gmail.com>
2021-12-27 01:28:13 -0800

    9.5.3

--- app.js
+++ app.js
@@ -146,8 +146,19 @@ promptUser()
   .then(promptProject)
   .then((portfolioData) => {
     const pageHTML = generatePage(portfolioData);
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
+    fs.writeFile("./dist/index.html", pageHTML, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
       console.log("Page created! Check out index.html to see the output!");
+
+      fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Style sheet copied successfully!");
+      });
     });
   });
